db8ebd2a7fb5dd2f5d840508bd7607a5f48b8402
lil137 <55464254+lil137@users.noreply.github.com>
2019-10-31 14:29:18 +0000

    Create the repository and add necessary tools

---
+++ test2.js
@@ -0,0 +1,16 @@
+
+const Octokit = require("@octokit/rest");
+const octokit = new Octokit();
+
+// Compare: https://developer.github.com/v3/repos/#list-organization-repositories
+octokit.repos
+  .listForOrg({
+    org: "octokit",
+    type: "public"
+  })
+  .then(({ data }) => {
+    // handle data
+  });
+
+
+console.log("程序执行结束!");
