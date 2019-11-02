eaf0207db7a604b83bdd8f71e48b1226cae5abbe
lil137 <scyyh2@nottingham.edu.cn>
2019-11-02 23:35:57 +0000

    Edit the file and test some new functions

--- GithubAccess/src/Getdata.java
+++ GithubAccess/src/Getdata.java
@@ -10,20 +10,31 @@ public class Getdata {
 
 		//Basic authentication		
 		GitHubClient client = new GitHubClient();
-		//client.setCredentials("lil137", "tckt31079");
-		client.setOAuth2Token("bd7f94f8e5038023818180598e2e2dfe8d025691");
+		client.setCredentials("lil137", "tckt31079");
+		//client.setOAuth2Token("bd7f94f8e5038023818180598e2e2dfe8d025691");
+		CollaboratorService collaboratorservice = new CollaboratorService(client);
+		RepositoryId repository = new RepositoryId("google-research","google-research");
+		repository = RepositoryId.createFromUrl("https://github.com/google-research/google-research");
+		RepositoryService githubservice = new RepositoryService();
+		int contributor_count = 0;
+		for (Contributor contributor :githubservice.getContributors(repository,false)) {
+			contributor_count++;
+			System.out.println(contributor.getName());
+		}
+		System.out.println(contributor_count);
+		//for (User user : collaboratorservice.getCollaborators(repository))
+		//	  System.out.println(user.getName());
 		
-		RepositoryService service = new RepositoryService();
-		for (Repository repo : service.getRepositories("lil137"))
-		  System.out.println(repo.getName() + " Watchers: " + repo.getWatchers());
+		//RepositoryService service = new RepositoryService();
+		//for (Repository repo : service.getRepositories("lil137"))
+		//  System.out.println(repo.getName() + " Watchers: " + repo.getWatchers());
 		
 		//for (Repository repo : service.getCommit("google-research"))
 		//	  System.out.println(repo.getName() + " Watchers: " + repo.getWatchers());
 		//service.getLanguages("google-reasearch");
-		Repository Google_research = new Repository();
-		Google_research.setName("google-research");
-		for (String repo : service.getLanguages("Google-research"))
-			  System.out.println(repo);
+	
+		//for (String repo : service.getLanguages("Google-research"))
+		//	  System.out.println(repo);
 	}
 
 	
